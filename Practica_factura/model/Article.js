6c18374c2edae08d08a0ebdbec3ce46c57321958
SergiSanahuja <80123937+SergiSanahuja@users.noreply.github.com>
2023-03-03 16:35:28 +0100

    Update Article.js

--- Practica_factura/model/Article.js
+++ Practica_factura/model/Article.js
@@ -4,21 +4,15 @@
  * @description Classe que representa un article de la factura
  */
 
+export class Article {
 /**
  * @param {string} codi - Codi de l'article (identificador del producte)
  * @param {string} nom - Nom de l'article
  * @param {number} unitat - Unitats de l'article (quantitat dels productes)
  * @param {number} preu - Preu de l'article (preu unitari del producte)
  * @param {number} subtotal - Subtotal de l'article (preu * unitats)
- * @method getCodi - Retorna el codi de l'article
- * @method getNom - Retorna el nom de l'article
- * @method getUnitat - Retorna les unitats de l'article
- * @method getPreu - Retorna el preu de l'article
- * @method getSubtotal - Retorna el subtotal de l'article
- * @method crearArticle - Crea un nou article
  * @returns {Article} - Retorna un objecte de tipus Article
  */
-export class Article {
     #codi;
     #nom;
     #unitats;
@@ -34,13 +28,43 @@ export class Article {
     }
 
     // Getters retornen els valors de les propietats
+    /**
+     * RETORNA EL CODI DE L'ARTICLE
+     * @returns {string} - Retorna el codi de l'article
+     */
     getCodi() {return this.#codi};
+
+    /**
+     *  RETORNA EL NOM DE L'ARTICLE
+     * @returns {string} - Retorna el nom de l'article
+     */
     getNom() {return this.#nom};
+
+    /**
+     * RETORNA LES UNITATS DE L'ARTICLE
+     * @returns {number} - Retorna les unitats de l'article
+     */
     getUnitat() {return this.#unitats};
+
+    /**
+     * RETORNA EL PREU DE L'ARTICLE
+     * @returns {number} - Retorna el preu de l'article
+     */
+
     getPreu() {return parseFloat(this.#preu)};
+    
+    /**
+     * RETORNA EL SUBTOTAL DE L'ARTICLE
+     * @returns {number} - Retorna el subtotal de l'article
+     */
     getSubtotal() {return parseFloat(this.#subtotal)};
 
-
+    /**
+     * Crear un article
+     * @param {String} nom 
+     * @param {Number} unitat 
+     * @param {Number} preu 
+     */
     crearArticle(nom, unitat, preu) {
         new Article(nom, unitat, preu);
     }
